ce8aac5250e6da5241840b39555b7493a039c18a
ADISH.A.P <helloadish007@gmail.com>
2018-10-23 06:53:25 +0000

    Update STUDENT.sql

--- STUDENT.sql
+++ STUDENT.sql
@@ -23,5 +23,16 @@ CREATE TABLE class_(
            ('ram','p','address','+910000000000','email',1),
            ('hello','g','address','+910000000000','email',1),
            ('y','kjhd','address','+910000000000','email',1);
-            
-            
+  create table company(
+    emp_id int primary key not null ,
+    name text not null,
+    age smallint not null check(age>=18),
+    address varchar(50),
+    salary real,
+    join_date date
+    );
+    create table department(
+      id int primary key not null,
+      dept varchar(50) not null,
+      emp_id int not null
+      );
